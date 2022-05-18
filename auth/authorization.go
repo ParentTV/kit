b251e9d07cf5b79d1a9d8acbd23072444e942e90
Jack Butler <jack@jackhas.codes>
2022-05-18 15:27:04 +1000

    call actual auth service

--- auth/authorization.go
+++ auth/authorization.go
@@ -1,17 +1,39 @@
 package auth
 
-import "github.com/ParentTV/kit/event_bus"
+import (
+	"encoding/base64"
+	"fmt"
+	"github.com/ParentTV/kit/event_bus"
+	"log"
+	"net/http"
+	"os"
+)
 
 type Auth struct {
-	eb *event_bus.EventBus
+	eb         *event_bus.EventBus
+	url        string
+	authstring string
 }
 
 func NewAuth(eb *event_bus.EventBus) *Auth {
-	return &Auth{eb}
+	a := base64.StdEncoding.EncodeToString([]byte(os.Getenv("AUTH_SERVICE_USER") + ":" + os.Getenv("AUTH_SERVICE_PASS")))
+	return &Auth{eb, os.Getenv("AUTH_SERVICE_URL"), "Basic " + a}
 }
 
 func (a *Auth) IsAuthorized(id string, perm string) bool {
-	return true
+	reqUrl := fmt.Sprintf("%s?id=%s&perm=%s", a.url, id, perm)
+	req, err := http.NewRequest("GET", reqUrl, nil)
+	if err != nil {
+		log.Println("Error on request.\n[ERROR] -", err)
+	}
+	// add authorization header to the req
+	req.Header.Add("Authorization", a.authstring)
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println("Error on response.\n[ERROR] -", err)
+	}
+	return resp.StatusCode == 200
 }
 
 func (a *Auth) RegisterPermissions(perms []string) {
